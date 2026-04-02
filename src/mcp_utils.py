9643788986588510c36a4496745dcdb7bc22350d
SV Madhava Reddy <madhavtheiiitan@gmail.com>
2026-04-01 21:13:37 -0600

    mcp servers access fix

--- src/mcp_utils.py
+++ src/mcp_utils.py
@@ -8,9 +8,9 @@ def enabled_mcp_connections_list() -> List[Dict[str, Any]]:
   # Using host field to mitigate DNS Rebinding Protection
   return [
     { "name": "aws-api-mcp-server", "url": "http://mcp-servers:8000/mcp", "host": "127.0.0.1:8000" },
-    { "name": "aws-documentation-mcp-server", "url": "http://mcp-servers:8001/mcp", "host": "localhost:8001" },
-    { "name": "aws-pricing-mcp-server", "url": "http://mcp-servers:8002/mcp", "host": "localhost:8002" },
-    { "name": "aws-billing-cost-management-mcp-server", "url": "http://mcp-servers:8003/mcp", "host": "localhost:8003" },
-    { "name": "aws-cloudtrail-mcp-server", "url": "http://mcp-servers:8004/mcp", "host": "localhost:8004" },
-    { "name": "aws-iac-mcp-server", "url": "http://mcp-servers:8005/mcp", "host": "localhost:8005" }
+    { "name": "aws-documentation-mcp-server", "url": "http://mcp-servers:8001/mcp", "host": "127.0.0.1:8001" },
+    { "name": "aws-pricing-mcp-server", "url": "http://mcp-servers:8002/mcp", "host": "127.0.0.1:8002" },
+    { "name": "aws-billing-cost-management-mcp-server", "url": "http://mcp-servers:8003/mcp", "host": "127.0.0.1:8003" },
+    { "name": "aws-cloudtrail-mcp-server", "url": "http://mcp-servers:8004/mcp", "host": "127.0.0.1:8004" },
+    { "name": "aws-iac-mcp-server", "url": "http://mcp-servers:8005/mcp", "host": "127.0.0.1:8005" }
   ]
